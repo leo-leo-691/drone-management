484dde2b86f2679aa44ff28c592fc474bc50f009
leo-leo-691 <arindamgogoi.cc2023@gmail.com>
2026-03-06 21:11:22 +0530

    Update Photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import groupPhoto from "./committee.png";
+import groupPhoto from "./committee.jpeg";
 import logoLightNoBg from "./logo-light-no-bg.png";
 import collegeLogo from "./college-logo.jpg";
 export { logoLightNoBg, collegeLogo, groupPhoto };
